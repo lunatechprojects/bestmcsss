14d68a55b12f981c0cbd9d45d0923497690ab2f8
lunatechprojects <159207019+lunatechprojects@users.noreply.github.com>
2026-03-06 10:57:24 +0300

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,1 +1,19 @@
+<?php
 
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$to = "jethro256qwerty@gmail.com";   // email of the site designer
+$subject = "New Contact Form Message";
+
+$body = "Name: $name\n";
+$body .= "Email: $email\n";
+$body .= "Message:\n$message";
+
+$headers = "From: $email";
+
+mail($to, $subject, $body, $headers);
+
+echo "Message sent successfully!";
+?>
